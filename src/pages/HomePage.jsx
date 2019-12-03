53bb0885333d3c98e8dc9c7d53716186d4820aa9
Nikki Kerns <nkernns@admorgan.com>
2019-12-03 13:41:39 -0500

    updated logic from class

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,36 +1,71 @@
 import React, { useState } from 'react'
 
 const HomePage = () => {
-  const [input, setInput] = useState([])
-
-  const inputClicked = val => {
-    setInput(prev => {
-      return prev + val
+  const [display, setDisplay] = useState('')
+  const [firstNumber, setFirstNumber] = useState(0)
+  const [operand, setOperand] = useState('')
+  const [runningTotal, setRunningTotal] = useState(0)
+  const displayClicked = val => {
+    setDisplay(prev => {
+      return prev + val.toString()
     })
   }
+  // const calculate = () => {
+  //   console.log('calculating')
+  //   if (display[1] === '-') {
+  //     console.log('yes')
+  //     setDisplay(parseInt(display[0]) - parseInt(display[2]))
+  //   } else if (display[1] === '+') {
+  //     setDisplay(parseInt(display[0]) + parseInt(display[2]))
+  //   } else if (display[1] === '*') {
+  //     setDisplay(parseInt(display[0]) * parseInt(display[2]))
+  //   } else if (display[1] === '/') {
+  //     setDisplay(parseInt(display[0]) / parseInt(display[2]))
+  //   }
+  // }
+  const operandButtonPressed = op => {
+    setOperand(op)
+    setFirstNumber(display)
+    setDisplay('')
+  }
 
-  const calculate = () => {
-    console.log('calculating')
-    if (input[1] === '-') {
-      console.log('yes')
-      setInput(parseInt(input[0]) - parseInt(input[2]))
-    } else if (input[1] === '+') {
-      setInput(parseInt(input[0]) + parseInt(input[2]))
-    } else if (input[1] === '*') {
-      setInput(parseInt(input[0]) * parseInt(input[2]))
-    } else if (input[1] === '/') {
-      setInput(parseInt(input[0]) / parseInt(input[2]))
+  const getResult = operand => {
+    let total = runningTotal
+    console.log({ total, display, operand })
+    switch (operand) {
+      case '+':
+        total = parseInt(firstNumber) + parseInt(display)
+        break
+      case '-':
+        total = parseInt(firstNumber) - parseInt(display)
+        break
+      case '*':
+        total = parseInt(firstNumber) * parseInt(display)
+        break
+      case '/':
+        total = parseInt(firstNumber) / parseInt(display)
+        break
     }
+    return total
+  }
+
+  const calculateResult = () => {
+    let total = getResult(operand)
+    console.log({ total })
+    setDisplay(total)
+    setRunningTotal(total)
   }
 
   const clearState = () => {
-    setInput([])
+    setDisplay([])
+    setFirstNumber(0)
+    setOperand('')
   }
   return (
     <>
       <div className="app">
         <div className="calc-wrapper">
-          <div className="input">{input}</div>
+          <div className="display">{display}</div>
 
           <div className="row">
             <button className="clear-button" onClick={() => clearState()}>
@@ -38,56 +73,68 @@ const HomePage = () => {
             </button>
           </div>
           <div className="row">
-            <button className="btn-numbers" onClick={() => inputClicked(7)}>
+            <button className="btn-numbers" onClick={() => displayClicked(7)}>
               7
             </button>
-            <button className="btn-numbers" onClick={() => inputClicked(8)}>
+            <button className="btn-numbers" onClick={() => displayClicked(8)}>
               8
             </button>
-            <button className="btn-numbers" onClick={() => inputClicked(9)}>
+            <button className="btn-numbers" onClick={() => displayClicked(9)}>
               9
             </button>
-            <button className="btn-none" onClick={() => inputClicked('/')}>
+            <button
+              className="btn-none"
+              onClick={() => operandButtonPressed('/')}
+            >
               /
             </button>
           </div>
 
           <div className="row">
-            <button className="btn-numbers" onClick={() => inputClicked(4)}>
+            <button className="btn-numbers" onClick={() => displayClicked(4)}>
               4
             </button>
-            <button className="btn-numbers" onClick={() => inputClicked(5)}>
+            <button className="btn-numbers" onClick={() => displayClicked(5)}>
               5
             </button>
-            <button className="btn-numbers" onClick={() => inputClicked(6)}>
+            <button className="btn-numbers" onClick={() => displayClicked(6)}>
               6
             </button>
-            <button className="btn-none" onClick={() => inputClicked('*')}>
+            <button
+              className="btn-none"
+              onClick={() => operandButtonPressed('*')}
+            >
               x
             </button>
           </div>
           <div className="row">
-            <button className="btn-numbers" onClick={() => inputClicked(1)}>
+            <button className="btn-numbers" onClick={() => displayClicked(1)}>
               1
             </button>
-            <button className="btn-numbers" onClick={() => inputClicked(2)}>
+            <button className="btn-numbers" onClick={() => displayClicked(2)}>
               2
             </button>
-            <button className="btn-numbers" onClick={() => inputClicked(3)}>
+            <button className="btn-numbers" onClick={() => displayClicked(3)}>
               3
             </button>
-            <button className="btn-none" onClick={() => inputClicked('-')}>
+            <button
+              className="btn-none"
+              onClick={() => operandButtonPressed('-')}
+            >
               -
             </button>
           </div>
           <div className="row">
-            <button className="btn-numbers" onClick={() => inputClicked(0)}>
+            <button className="btn-numbers" onClick={() => displayClicked(0)}>
               0
             </button>
-            <button className="btn-none" onClick={() => calculate()}>
+            <button className="btn-none" onClick={() => calculateResult()}>
               =
             </button>
-            <button className="btn-none" onClick={() => inputClicked('+')}>
+            <button
+              className="btn-none"
+              onClick={() => operandButtonPressed('+')}
+            >
               +
             </button>
           </div>
